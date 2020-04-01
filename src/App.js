1c07abc53cbe2c25bdcb79b95aef8c992fec0f1a
Bradley-B <brad.boxer1@gmail.com>
2020-03-31 21:59:35 -0400

    Landing Page
    Basic homepage with the three main buttons and helper login in the corner.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';  // please replace this logo with the svg one
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App homepage">
+          <button className="homepage-sign-in-btn">Helper Log In</button>
+          <img src={logo} alt="logo" />
+          <div className="homepage-buttons">
+              <button>Questions</button>
+              <button>Help Articles</button>
+              <button>Search All</button>
+          </div>
+      </div>
   );
 }
 
