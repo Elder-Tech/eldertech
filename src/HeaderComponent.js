f224bf31cabbd13554f35cd750c3b925d21265e6
Bradley-B <brad.boxer1@gmail.com>
2020-04-03 19:19:14 -0400

    Add Article Picture and Question Mark Picture To Header
    I don't have the image file for the home picture, so someone else will have to add that.

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {BrowserRouter as Router, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import logo from "./logo.png";
+import article_img from "./article.png";
+import question_img from "./question.png";
 
 export default class HeaderComponent extends React.Component {
     constructor(props) {
@@ -11,8 +13,8 @@ export default class HeaderComponent extends React.Component {
             <img src={logo} alt="logo" />
             <nav>
                 <Link to="/"><button>Home</button></Link>
-                <Link to="/articles"><button>Articles</button></Link>
-                <Link to="/questions"><button>Questions</button></Link>
+                <Link to="/articles"><img src={article_img} alt="newspaper article"/><button>Articles</button></Link>
+                <Link to="/questions"><img src={question_img} alt="question mark"/><button>Questions</button></Link>
                 <form id="search">
                     <input type="text" placeholder="Search"/>
                 </form>
