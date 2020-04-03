ec78c896126129485b7f5adaa2e7cc9d30d961b8
Bradley-B <brad.boxer1@gmail.com>
2020-04-02 20:16:29 -0400

    Controlled Login Component
    Control the login form so we have access to the results. On form submit, the string 'true' is stored for the key 'loggedIn' in localStorage. The user is also redirected back to the homepage.

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import HeaderComponent from "./HeaderComponent";
+import { Redirect } from 'react-router-dom';
 
 export default class LoginComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {redirect: null, values: {username: '', password: ''}};
+    }
+
+    handleChange(event) {
+        let form_values = this.state.values;
+        form_values[event.target.id] = event.target.value;
+        this.setState({values: form_values});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        localStorage.setItem('loggedIn', 'true');
+        this.setState({redirect: '/'});
     }
 
     render() {
+        if(this.state.redirect) {
+            return <Redirect to={this.state.redirect} />
+        }
+
         return <div className="App">
             <HeaderComponent/>
             <div className="login-page">
                 <h1>Helper Login</h1>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="text" placeholder="Password" />
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.values.username} id="username" type="text" placeholder="Username" required={true} onChange={this.handleChange}/>
+                    <input value={this.state.values.password} id="password" type="password" placeholder="Password" required={true} onChange={this.handleChange}/>
                     <input type="submit" value="Login"/>
                 </form>
             </div>
