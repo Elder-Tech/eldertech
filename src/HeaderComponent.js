b2f4a8fb340ea4e780a419f55ad1ceb119e37923
Noah K <nwk6661@rit.edu>
2020-04-13 20:44:14 -0400

    Fix search view, add home icon

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import logo from "./logo.png";
 import article_img from "./article.png";
 import question_img from "./question.png";
+import home_img from "./home.png";
 
 export default class HeaderComponent extends React.Component {
     constructor(props) {
@@ -12,7 +13,7 @@ export default class HeaderComponent extends React.Component {
         return <header className="App-header">
             <img src={logo} alt="logo" />
             <nav>
-                <Link to="/"><button>Home</button></Link>
+                <Link to="/"><img src={home_img} alt="house"/><button>Home</button></Link>
                 <Link to="/articles"><img src={article_img} alt="newspaper article"/><button>Articles</button></Link>
                 <Link to="/questions"><img src={question_img} alt="question mark"/><button>Questions</button></Link>
                 <form id="search">
